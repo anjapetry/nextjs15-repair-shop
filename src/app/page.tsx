31e858289954527fd46bc642186e6f53182e80e2
Anja Petry <anjapetry@protonmail.com>
2024-10-23 22:37:51 +0200

    style: update fonts in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,29 @@
 import Link from "next/link";
 
 export default function Home() {
-    return (
-        <div className='bg-black bg-home-img bg-cover bg-center'>
-            <main className='flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh'>
-                <div className='flex flex-col gap-6 p-12 rounded-xl bg-black/90 w-4/5sm:max-w-96 mx-auto text-white sm:text-2xl'>
-                    <h1 className="text-4xl font-bold text-white">
-                        Dave&apos;s Computer
-                        <br />
-                        Repair Shop
-                    </h1>
-                    <address>
-                        555 Gateway Lane
-                        <br />
-                        Kansas City,, KS 55555
-                    </address>
-                    <p>
-                        <Link
-                            href='tel: 555-555-5555'
-                            className='hover:underline'
-                        >
-                            555-555-5555
-                        </Link>
-                    </p>
-                </div>
-            </main>
+  return (
+    <div className="bg-black bg-home-img bg-cover bg-center">
+      <main className="mx-auto flex h-dvh max-w-5xl flex-col justify-center text-center">
+        <div className="w-4/5sm:max-w-96 mx-auto flex flex-col gap-6 rounded-xl bg-black/90 p-12 text-white sm:text-2xl">
+          <h1 className="font-[family-name:var(--font-geist-sans)] text-4xl font-bold">
+            Dave&apos;s Computer
+            <br />
+            Repair Shop
+          </h1>
+          <div className="flex flex-col gap-2 font-[family-name:var(--font-geist-mono)]">
+            <address>
+              555 Gateway Lane
+              <br />
+              Kansas City, KS 55555
+            </address>
+            <p>
+              <Link href="tel: 555-555-5555" className="hover:underline">
+                tel: 555-555-5555
+              </Link>
+            </p>
+          </div>
         </div>
-    );
+      </main>
+    </div>
+  );
 }
