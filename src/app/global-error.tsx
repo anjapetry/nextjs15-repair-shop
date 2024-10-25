252cb91ef52027aed8f7414224d2d4ab335c20ac
Anja Petry <anjapetry@protonmail.com>
2024-10-25 22:12:13 +0200

    test: implement Sentry in src/app/global-error.tsx

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,5 +1,7 @@
 "use client"; // Error boundaries must be Client Components
 
+import * as Sentry from "@sentry/nextjs";
+import { useEffect } from "react";
 export default function GlobalError({
   error,
   reset,
@@ -7,9 +9,15 @@ export default function GlobalError({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+    Sentry.captureException(error);
+  }, [error]);
+
   return (
     // global-error must include html and body tags
-    <html>
+    <html lang="en">
       <body>
         <h2>Something went wrong!</h2>
         <button onClick={() => reset()}>Try again</button>
