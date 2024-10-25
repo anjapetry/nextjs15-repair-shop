ff2e399a05fcb9a25a9b1ccd2f72c8e2c97de7df
Anja Petry <anjapetry@protonmail.com>
2024-10-25 21:15:50 +0200

    chore: update next.config.ts with Sentry config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
+import { withSentryConfig } from "@sentry/nextjs";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {};
 
-export default nextConfig;
+// Make sure adding Sentry options is the last code to run before exporting
+export default withSentryConfig(nextConfig, {
+  org: "captogether",
+  project: "nextjs15-repair-shop",
+
+  // An auth token is required for uploading source maps.
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+
+  silent: false, // Can be used to suppress logs
+
+  hideSourceMaps: true,
+
+  disableLogger: true,
+});
