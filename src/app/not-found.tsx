de780e1f6e37f66c7fb50c3c5a3069094352984a
Anja Petry <anjapetry@protonmail.com>
2024-10-27 21:23:56 +0100

    feat: customise not-found.tsx with an image

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,26 @@
-import Link from "next/link";
+import Image from "next/image";
+
+export const metadata = {
+  title: "Page Not Found",
+};
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="w-full px-2">
+      <div className="mx-auto flex flex-col items-center justify-center gap-5 py-4">
+        <h1 className="text-2xl font-semibold">Page Not Found</h1>
+        <Image
+          className="m-0 rounded-xl"
+          src="/images/not-found-1024x1024.png"
+          width={300}
+          height={300}
+          quality={90}
+          priority={true}
+          sizes="300px"
+          alt="Page Not Found"
+          title="Page Not Found"
+        />
+      </div>
     </div>
   );
 }
