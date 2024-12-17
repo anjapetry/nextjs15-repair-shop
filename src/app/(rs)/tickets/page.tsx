3270a6eb474d6903d46409178df8a34eb18d2581
Anja Petry <anjapetry@protonmail.com>
2024-12-17 18:59:21 +0100

    feat: update src/app/(rs)/tickets/page.tsx

--- src/app/(rs)/tickets/page.tsx
+++ src/app/(rs)/tickets/page.tsx
@@ -1,7 +1,27 @@
+import TicketSearch from "@/app/(rs)/tickets/TicketSearch";
+
 export const metadata = {
-  title: "Tickets",
+  title: "Ticket Search",
 };
 
-export default function Tickets() {
-  return <h1>Tickets Page</h1>;
+export default async function Tickets({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+  const { searchText } = await searchParams;
+
+  if (!searchText) {
+    // query default results
+    return (
+      <>
+        <TicketSearch />
+        <p>{JSON.stringify("placeholder")}</p>
+      </>
+    );
+  }
+
+  // query search results
+
+  // return search result
 }
