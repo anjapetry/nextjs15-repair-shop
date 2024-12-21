fbb5fe8c3c4cbba27cdc2b698235819610eb4318
Anja Petry <anjapetry@protonmail.com>
2024-12-21 23:01:30 +0100

    test: measure performance with Sentry.startInactiveSpan

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,5 +1,6 @@
 import CustomerSearch from "@/app/(rs)/customers/CustomerSearch";
 import { getCustomerSearchResults } from "@/lib/queries/getCustomerSearchResults";
+import * as Sentry from "@sentry/nextjs";
 
 export const metadata = {
   title: "Customers",
@@ -14,8 +15,13 @@ export default async function Customers({
 
   if (!searchText) return <CustomerSearch />;
 
+  // measure performance with a span
+  const span = Sentry.startInactiveSpan({
+    name: 'getCustomerSearchResults-1'
+  })
   // query database
   const results = await getCustomerSearchResults(searchText);
+  span.end();s
 
   // return results
   return (
