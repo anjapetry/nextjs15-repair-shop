0cd2c67b355bfd72e4d7b8c5e1b248f967d83f39
Anja Petry <anjapetry@protonmail.com>
2024-12-22 00:01:24 +0100

    feat: add CustomerTable.tsx to src/app/(rs)/customers/page.tsx

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,6 +1,7 @@
 import CustomerSearch from "@/app/(rs)/customers/CustomerSearch";
 import { getCustomerSearchResults } from "@/lib/queries/getCustomerSearchResults";
 import * as Sentry from "@sentry/nextjs";
+import CustomerTable from "@/app/(rs)/customers/CustomerTable";
 
 export const metadata = {
   title: "Customers",
@@ -27,7 +28,11 @@ export default async function Customers({
   return (
     <>
       <CustomerSearch />
-      <p>{JSON.stringify(results)}</p>
+      {results.length ? (
+        <CustomerTable data={results} />
+      ) : (
+        <p className="mt-4">No results found</p>
+      )}
     </>
   );
 }
