c4ad5d34ec656edc351c53518106bb6f3d4aa05b
Anja Petry <anjapetry@protonmail.com>
2025-01-15 17:19:09 +0100

    refactor:change links to /home to /tickets as app's homepage in Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,10 +12,10 @@ export function Header() {
     <header className="sticky top-0 z-20 h-12 animate-slide border-b bg-background p-2">
       <div className="flex h-8 w-full items-center justify-between">
         <div className="flex items-center gap-2">
-          <NavButton href="/home" label="Home" icon={HomeIcon} />
+          <NavButton href="/tickets" label="Home" icon={HomeIcon} />
 
           <Link
-            href="/home"
+            href="/tickets"
             className="ml-0 flex items-center justify-center gap-2"
             title="Home"
           >
