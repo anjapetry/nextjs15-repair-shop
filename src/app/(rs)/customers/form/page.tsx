d21e804f229055753080136de975660a5af1467b
Anja Petry <anjapetry@protonmail.com>
2025-01-23 17:15:44 +0100

    fix: update src/app/(rs)/customers/form/page.tsx

--- src/app/(rs)/customers/form/page.tsx
+++ src/app/(rs)/customers/form/page.tsx
@@ -1,57 +1,56 @@
 import { getCustomer } from "@/lib/queries/getCustomer";
 import { BackButton } from "@/components/BackButton";
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from "@sentry/nextjs"
 import CustomerForm from "@/app/(rs)/customers/form/CustomerForm";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export async function generateMetaData({
-  searchParams,
+export async function generateMetadata({
+    searchParams,
 }: {
-  searchParams: Promise<{ [key: string]: string | undefined }>;
+    searchParams: Promise<{ [key: string]: string | undefined }>
 }) {
-  const { customerId } = await searchParams;
+    const { customerId } = await searchParams
 
-  if (!customerId) return { title: "New Customer" };
-  return { title: `Edit Customer #${customerId}` };
+    if (!customerId) return { title: "New Customer" }
+
+    return { title: `Edit Customer #${customerId}` }
 }
 
 export default async function CustomerFormPage({
-  searchParams,
+    searchParams,
 }: {
-  searchParams: Promise<{ [key: string]: string | undefined }>;
+    searchParams: Promise<{ [key: string]: string | undefined }>
 }) {
-  try {
-    const { getPermission } = getKindeServerSession();
-    const managerPermission = await getPermission("manager");
-    const isManager = managerPermission?.isGranted;
+    try {
+        const { getPermission } = getKindeServerSession()
+        const managerPermission = await getPermission("manager")
+        const isManager = managerPermission?.isGranted
 
-    const { customerId } = await searchParams;
+        const { customerId } = await searchParams
 
-    // Edit customer form
-    if (customerId) {
-      const customer = await getCustomer(parseInt(customerId));
+        // Edit customer form 
+        if (customerId) {
+            const customer = await getCustomer(parseInt(customerId))
 
-      if (!customer) {
-        return (
-          <>
-            <h2 className="mb-2 text-2xl">
-              Customer ID #{customerId} not found
-            </h2>
-            <BackButton title="Go Back" variant="default" />
-          </>
-        );
-      }
-      console.log(customer);
-      // put customer form component
-      return <CustomerForm key={customerId} customer={customer} isManager={isManager} />;
-    } else {
-      // new customer form component
-      return <CustomerForm key="new" isManager={isManager} />;
+            if (!customer) {
+                return (
+                    <>
+                        <h2 className="text-2xl mb-2">Customer ID #{customerId} not found</h2>
+                        <BackButton title="Go Back" variant="default" />
+                    </>
+                )
+            }
+            // put customer form component
+            return <CustomerForm key={customerId} isManager={isManager} customer={customer} />
+        } else {
+            // new customer form component
+            return <CustomerForm key="new" isManager={isManager} />
+        }
+
+    } catch (e) {
+        if (e instanceof Error) {
+            Sentry.captureException(e)
+            throw e
+        }
     }
-  } catch (e) {
-    if (e instanceof Error) {
-      Sentry.captureException(e);
-      throw e;
-    }
-  }
 }
